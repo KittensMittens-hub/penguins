a6b161785126c75d137161b967fc7af18ba754a5
Natalia <n.timkina2014@yandex.ru>
2025-09-04 21:31:43 +0300

    the beak length histogram was saved

--- penguins.R
+++ penguins.R
@@ -1,23 +1,15 @@
 data <- read.csv("penguins.csv")
-data$bill_length_mm
-
-data <- read.csv("penguins.csv")
-
-str(data)
 x <- data$bill_length_mm
 x <- as.numeric(data$bill_length_mm)
-hist(x, breaks = seq(40, 60, 4))
-hist(x, breaks = seq(40, 60, 2))
-hist(x, breaks = seq(40, 60, 1))
-
-
-data <- read.csv("penguins.csv")
-x <- data$bill_length_mm
-x <- as.numeric(data$bill_length_mm) 
 x <- na.omit(x)
 hist(x, breaks = seq(40, 60, 4))
 hist(x, breaks = seq(40, 60, 2))
 hist(x, breaks = seq(40, 60, 1))
-head(data)
 
-
+data <- read.csv("penguins.csv")
+x <- data$bill_length_mm
+x <- as.numeric(data$bill_length_mm)
+x <- na.omit(x)
+png("penguins-hist.png")
+hist(x, breaks = seq(40, 60, 2))
+dev.off()
